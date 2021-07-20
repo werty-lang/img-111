8855b40b4e04347e8af17777157ff57bc35c437a
werty-lang <84279588+werty-lang@users.noreply.github.com>
2021-07-20 18:09:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+    Height:300,
+    Width:310,
+    image_format: 'png',
+png_quality:90,
+ 
+constraints:{facingMode: "environment"}
+});
+
+camera =document.getElementById("camera")
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version',ml5.version);
+classifier=ml5.imageClassifier('MobileNet',modelLoaded);
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if (error){
+        console.error(error);
+} else{
+    console.log(results)
+    document.getElementById("object_name").innerHTML =results[0].label;
+}
+
+
+}
+
+function modelLoaded(){
+    console.log("Model is too bad")
+}
